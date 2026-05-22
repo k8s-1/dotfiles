5593c5b3b32bf070fd11ead7c875c5a4b7c5005f
k8s-1 <176627561+k8s-1@users.noreply.github.com>
2026-05-22 09:04:44 +0200

    chore: update file(s): .config/nvim/lua/config/autocmds.lua

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -13,6 +13,29 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 
+local function is_wsl()
+  local f = io.open('/proc/version', 'r')
+  if not f then return false end
+  local s = f:read('*a')
+  f:close()
+  return s:lower():find('microsoft') ~= nil
+end
+
+if is_wsl() then
+  vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+    pattern = "*",
+    callback = function()
+      local lines = vim.api.nvim_buf_get_lines(0, 0, 20, false)
+      for _, line in ipairs(lines) do
+        if line:find('\r') then
+          vim.opt_local.fileformat = "dos"
+          return
+        end
+      end
+    end,
+  })
+end
+
 -- " Use tabs for Go files (4 spaces wide)
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "go",
